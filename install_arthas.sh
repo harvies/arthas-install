38a5723d043be96c731542c15f3e1d63269ea09c
harvies <delverjw@gmail.com>
2020-08-16 14:28:50 +0800

    wget

--- install_arthas.sh
+++ install_arthas.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-curl -O https://arthas.aliyun.com/arthas-boot.jar
-mkdir /opt/arthas
-mv arthas-boot.jar /opt/arthas
-cp startup_arthas.sh /opt/arthas
+wget -p /opt/arthas https://arthas.aliyun.com/arthas-boot.jar
+wget -p /opt/arthas https://raw.githubusercontent.com/harvies/arthas-install/master/startup_arthas.sh
 ln -s /opt/arthas/startup_arthas.sh /usr/local/bin/arthas
