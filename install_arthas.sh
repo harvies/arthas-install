c517fb62ee02d877674b6986cc0499c125ece006
harvies <delverjw@gmail.com>
2020-08-21 23:49:20 +0800

    commit

--- install_arthas.sh
+++ install_arthas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /opt/arthas
 wget -O /opt/arthas/arthas-boot.jar https://arthas.aliyun.com/arthas-boot.jar
-wget -O /opt/arthas/startup_arthas.sh https://raw.githubusercontent.com/harvies/arthas-install/master/startup_arthas.sh
+wget -O /opt/arthas/startup_arthas.sh https://cdn.jsdelivr.net/gh/harvies/arthas-install@master/startup_arthas.sh
 ln -s /opt/arthas/startup_arthas.sh /usr/local/bin/arthas
 chmod +x /opt/arthas/startup_arthas.sh
 source /etc/profile
